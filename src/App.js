ea488e47593bd1c9ed3d44871a978fb47cb935d3
Yoonjung Rhew <koderhew@gmail.com>
2022-08-08 11:59:53 +0900

    created login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Sidebar from "./components/sidebar/sidebar";
 import Main from "./components/main/main";
 import styles from './app.module.css';
 import {useAppDispatch, useAppSelector} from "./store/hooks";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
 
 function App() {
 
   return (
     <div className={styles.app}>
-      <Sidebar />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        {/*<Route path="/login" element={<Login />}></Route>*/}
+      </Routes>
     </div>
   );
 }
